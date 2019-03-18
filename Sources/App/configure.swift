2c11b1a171b0d47057dc2b88ea9f3d78b173f388
Travis Bogosian <travis.m.bogosian@gmail.com>
2019-03-17 22:32:00 -0400

    Cleanup appearance

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,17 +4,20 @@ import Leaf
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    // Register providers first
+    try services.register(LeafProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
 
-    let leafProvider = LeafProvider()
-    try services.register(leafProvider)
+    // Use Leaf for rendering views
     config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 
-    let myService = NIOServerConfig.default(port: 8080)
-    services.register(myService)
-
+    // Register middleware
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    services.register(middlewares)
 }
